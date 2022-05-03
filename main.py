99f7c8f3e067f22aa912cf3ca235863ca2edcc0d
Karol <Karol@pc.com>
2022-05-03 19:26:18 +0200

    add requirements.txt

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 import requests
 
-url = 'https://reqbin.com/echo'
 
-x = requests.get(url)
 
-print(x.text)
+URL = 'https://reqbin.com/echo'
+
+def isResponse200(status_code):
+
+    if status_code == 200:
+        print("OK -200")
+        print(URL)
+    else:
+        print("status_code")
+
+
+def main ():
+    x = requests.get(URL)
+
+    # print(x.text)
+    # print(x.status_code)
+    isResponse200(x.status_code)
+
+
+if __name__ == '__main__':
+    main()
