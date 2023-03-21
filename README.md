cb05472a757dcaae40e7d104f311397c71331a88
Karol <klimunio@gmail.com>
2023-03-21 12:39:32 +0000

    add Readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # endpoints-scraper
+
+
+# Endpoint Checker
+This is a Python script that checks a list of endpoints on a given base URL and logs the status codes of the HTTP responses.
+
+# Getting Started
+Clone the repository to your local machine.
+
+Install the required dependencies by running `pip install -r requirements.txt`
+
+# Usage
+The script will check each endpoint in the Endpoints parameter and log the status code of the HTTP response for each endpoint. The log file will be saved to the path specified in the LogFile parameter.
+
+# Contributing
+If you find any issues with the script or have suggestions for improvement, feel free to open an issue or pull request in the GitHub repository.
+
+# License
+This project is licensed under the MIT License - see the LICENSE.md file for details.
--- main.py
+++ main.py
@@ -1,36 +1,46 @@
 import time
 import requests
 
-# DEFINE FUNCTION TO PROCESS EACH ENDPOINT
-def process_endpoint(URL, ENDPOINT, DELAY, LOG_FILE):
-    FULL_URL = URL + ENDPOINT
-    print("Checking: " + FULL_URL)
+class EndpointChecker:
+    def __init__(self, base_url, endpoints, delay, log_file):
+        self.base_url = base_url
+        self.endpoints = endpoints
+        self.delay = delay
+        self.log_file = log_file
     
-    try:
-        RESPONSE = requests.get(FULL_URL)
-        # CHECK IF RESPONSE IS VALID (STATUS CODE BETWEEN 200 AND 399)
-        if 200 <= RESPONSE.status_code < 400:
-            LOG_FILE.write("Endpoint: " + ENDPOINT + "\n")
-            LOG_FILE.write("URL: " + FULL_URL + "\n")
-            LOG_FILE.write("Status code: " + str(RESPONSE.status_code) + "\n")
-            LOG_FILE.write("\n")
-            print("Status code: " + str(RESPONSE.status_code))
-        else:
-            print("Invalid response: " + str(RESPONSE.status_code))
-    except requests.exceptions.RequestException as e:
-        print("Error: " + str(e))
-    
-    time.sleep(DELAY)
+    def process_endpoint(self, endpoint):
+        full_url = self.base_url + endpoint
+        print("Checking: " + full_url)
+        
+        try:
+            response = requests.get(full_url)
+            # check if response is valid (status code between 200 and 399)
+            if 200 <= response.status_code < 500:
+                self.log_file.write("Endpoint: " + endpoint + "\n")
+                self.log_file.write("URL: " + full_url + "\n")
+                self.log_file.write("Status code: " + str(response.status_code) + "\n")
+                self.log_file.write("\n")
+                print("Status code: " + str(response.status_code))
+            else:
+                print("Invalid response: " + str(response.status_code))
+        except requests.exceptions.RequestException as e:
+            print("Error: " + str(e))
+        
+        time.sleep(self.delay)
 
-# SET PARAMETERS
-URL = "http://example.com/"
-ENDPOINTS = ["api", "login", "admin", "users"]
-DELAY = 1
-LOG_FILE = open("log.txt", "w")
+    def check_all_endpoints(self):
+        for endpoint in self.endpoints:
+            self.process_endpoint(endpoint)
 
-# PROCESS EACH ENDPOINT
-for ENDPOINT in ENDPOINTS:
-    process_endpoint(URL, ENDPOINT, DELAY, LOG_FILE)
+        # Close log file
+        self.log_file.close()
 
-# CLOSE LOG FILE
-LOG_FILE.close()
+# set parameters
+base_url = "http://example.com/"
+endpoints = ["api", "login", "admin", "users"]
+delay = 1
+log_file = open("log.txt", "w")
+
+# Create an instance of EndpointChecker and call the check_all_endpoints method
+checker = EndpointChecker(base_url, endpoints, delay, log_file)
+checker.check_all_endpoints()
